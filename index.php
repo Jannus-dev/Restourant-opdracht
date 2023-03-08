a1f949d92408385b43e0e638e3222d96afe8728e
Jannus <jan.honing2@gmail.com>
2023-03-08 09:47:28 +0100

    nav

--- index.php
+++ index.php
@@ -5,8 +5,30 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300&display=swap" rel="stylesheet">
 </head>
 <body>
-    
+    <header>
+        <img id="logo" src="img/logo.png" alt="logo">
+        <nav>
+            <a href="#">Home</a>
+            <a href="#">Bestellen</a>
+            <a href="#">Reviews</a>
+            <a href="#">Over Ons</a>
+            <a href="#">Contact</a>
+        </nav>
+        <a href="#"><img src="img/account.png" alt="login"></a>
+    </header>`
+
+    <main>
+
+    </main>
+
+    <footer>
+        
+    </footer>
 </body>
 </html>
