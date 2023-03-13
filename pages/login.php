391861e0a5a2b5aa86dbf3b100ba7441ab3c9860
Jannus <jan.honing2@gmail.com>
2023-03-13 11:08:00 +0100

    log in page

--- pages/login.php
+++ pages/login.php
@@ -13,6 +13,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Fredoka+One&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="../css/login.css">
 </head>
 
 <body>
@@ -40,29 +41,30 @@
 
                                     <h2 class="fw-bold mb-2 text-uppercase">Login</h2>
                                     <p class="text-white-50 mb-5">Please enter your login and password!</p>
+                                    <form name="loginform" action="inlog.php" method="POST">
+                                        <div class="form-outline form-white mb-4">
+                                            <input type="text" name="username" id="username"
+                                                class="form-control form-control-lg" />
+                                            <label class="form-label" for="typeEmailX">Email</label>
+                                        </div>
 
-                                    <div class="form-outline form-white mb-4">
-                                        <input type="email" id="typeEmailX" class="form-control form-control-lg" />
-                                        <label class="form-label" for="typeEmailX">Email</label>
-                                    </div>
+                                        <div class="form-outline form-white mb-4">
+                                            <input type="password" id="password" name="password"
+                                                class="form-control form-control-lg" />
+                                            <label class="form-label" for="typePasswordX">Password</label>
+                                        </div>
 
-                                    <div class="form-outline form-white mb-4">
-                                        <input type="password" id="typePasswordX"
-                                            class="form-control form-control-lg" />
-                                        <label class="form-label" for="typePasswordX">Password</label>
-                                    </div>
+                                        <p class="small mb-5 pb-lg-2"><a class="text-white-50" href="#!">Forgot
+                                                password?</a></p>
 
-                                    <p class="small mb-5 pb-lg-2"><a class="text-white-50" href="#!">Forgot
-                                            password?</a></p>
+                                        <button class="btn btn-outline-light btn-lg px-5" type="submit" value="Submit">Login</button>
 
-                                    <button class="btn btn-outline-light btn-lg px-5" type="submit">Login</button>
-
-                                    <div class="d-flex justify-content-center text-center mt-4 pt-1">
-                                        <a href="#!" class="text-white"><i class="fab fa-facebook-f fa-lg"></i></a>
-                                        <a href="#!" class="text-white"><i
-                                                class="fab fa-twitter fa-lg mx-4 px-2"></i></a>
-                                        <a href="#!" class="text-white"><i class="fab fa-google fa-lg"></i></a>
-                                    </div>
+                                        <div class="d-flex justify-content-center text-center mt-4 pt-1">
+                                            <a href="#!" class="text-white"><i class="fab fa-facebook-f fa-lg"></i></a>
+                                            <a href="#!" class="text-white"><i
+                                                    class="fab fa-twitter fa-lg mx-4 px-2"></i></a>
+                                            <a href="#!" class="text-white"><i class="fab fa-google fa-lg"></i></a>
+                                        </div>
 
                                 </div>
 
