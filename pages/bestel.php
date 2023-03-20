88e75ff312c8a8882b0e94ebecec429f741f896a
Jannus <jan.honing2@gmail.com>
2023-03-20 10:01:18 +0100

    items vaan winkel wagen

--- pages/bestel.php
+++ pages/bestel.php
@@ -18,7 +18,7 @@
 </head>
 
 <body>
-<?php 
+    <?php
     include 'header.php';
     ?>
 
@@ -50,7 +50,7 @@
                             goed genoeg. Deze ingrediënten en de kunde van de brouwers zorgen voor goudgele kleur en de
                             mooie volle schuimkraag. De zachte smaak en de aangenaam bittere afdronk zijn karakteristiek
                             voor Hertog Jan Pilsener.</p>
-                            <p class="prijs">€1,75</p>
+                        <p class="prijs">€1,75</p>
                         <a href="" id="knop">Voeg Toe</a>
                     </div>
                 </div>
@@ -65,7 +65,7 @@
                             goed genoeg. Deze ingrediënten en de kunde van de brouwers zorgen voor goudgele kleur en de
                             mooie volle schuimkraag. De zachte smaak en de aangenaam bittere afdronk zijn karakteristiek
                             voor Hertog Jan Pilsener.</p>
-                            <p class="prijs">€1,75</p>
+                        <p class="prijs">€1,75</p>
                         <a href="" id="knop">Voeg Toe</a>
                     </div>
                 </div>
@@ -80,7 +80,7 @@
                             goed genoeg. Deze ingrediënten en de kunde van de brouwers zorgen voor goudgele kleur en de
                             mooie volle schuimkraag. De zachte smaak en de aangenaam bittere afdronk zijn karakteristiek
                             voor Hertog Jan Pilsener.</p>
-                            <p class="prijs">€1,75</p>
+                        <p class="prijs">€1,75</p>
                         <a href="" id="knop">Voeg Toe</a>
                     </div>
                 </div>
@@ -95,26 +95,31 @@
                             goed genoeg. Deze ingrediënten en de kunde van de brouwers zorgen voor goudgele kleur en de
                             mooie volle schuimkraag. De zachte smaak en de aangenaam bittere afdronk zijn karakteristiek
                             voor Hertog Jan Pilsener.</p>
-                            <p class="prijs">€1,75</p>
+                        <p class="prijs">€1,75</p>
                         <a href="" id="knop">Voeg Toe</a>
                     </div>
                 </div>
             </div>
         </section>
 
-        <?php 
-    include 'footer.php';
-    ?>
-    <a href="#" id="mand">
-        <div class="mand"><svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor"
-                class="bi bi-cart4" viewBox="0 0 16 16">
-                <path
-                    d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l.5 2H5V5H3.14zM6 5v2h2V5H6zm3 0v2h2V5H9zm3 0v2h1.36l.5-2H12zm1.11 3H12v2h.61l.5-2zM11 8H9v2h2V8zM8 8H6v2h2V8zM5 8H3.89l.5 2H5V8zm0 5a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z" />
-            </svg></div>
-    </a>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous">
-    </script>
+        <?php
+        include 'footer.php';
+        ?>
+
+        <a href="#" id="mand">
+            <div class="mand"><svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor"
+                    class="bi bi-cart4" viewBox="0 0 16 16">
+                    <path
+                        d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l.5 2H5V5H3.14zM6 5v2h2V5H6zm3 0v2h2V5H9zm3 0v2h1.36l.5-2H12zm1.11 3H12v2h.61l.5-2zM11 8H9v2h2V8zM8 8H6v2h2V8zM5 8H3.89l.5 2H5V8zm0 5a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z" />
+                </svg></div>
+        </a>
+        <div class="aantal">
+            <p>0</p>
+        </div>
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous">
+        </script>
 
 </body>
 
